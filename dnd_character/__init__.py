c3364ff2749d09f738357bd52e29ac0add0a9bef
tassaron2 <rutachigal@gmail.com>
2020-11-04 22:23:36 -0500

    factories for each class

--- dnd_character/__init__.py
+++ dnd_character/__init__.py
@@ -8,11 +8,12 @@ __author__ = "Brianna Rainey"
 __copyright__ = "Copyright 2019-2020 Brianna Rainey & Markis Cook"
 __credits__ = ["Brianna Rainey (Current Programmer)", "Markis Cook (Original Creator)"]
 __license__ = "EPL-2.0"
-__version__ = "20.11.03"
+__version__ = "20.11.04"
 __maintainer__ = "Brianna Rainey"
 
 
 from .character import Character as CharacterObj
+from .classes import *
 
 
 def Character(**kwargs):
--- dnd_character/classes.py
+++ dnd_character/classes.py
@@ -1,1 +1,50 @@
-from dnd_character.SRD import SRD_classes as CLASSES
+from .SRD import SRD_classes as CLASSES
+from .character import Character
+
+
+def Barbarian(**kwargs):
+    return Character(classs=CLASSES["barbarian"], **kwargs)
+
+
+def Bard(**kwargs):
+    return Character(classs=CLASSES["bard"], **kwargs)
+
+
+def Cleric(**kwargs):
+    return Character(classs=CLASSES["cleric"], **kwargs)
+
+
+def Druid(**kwargs):
+    return Character(classs=CLASSES["druid"], **kwargs)
+
+
+def Fighter(**kwargs):
+    return Character(classs=CLASSES["fighter"], **kwargs)
+
+
+def Monk(**kwargs):
+    return Character(classs=CLASSES["monk"], **kwargs)
+
+
+def Paladin(**kwargs):
+    return Character(classs=CLASSES["paladin"], **kwargs)
+
+
+def Ranger(**kwargs):
+    return Character(classs=CLASSES["ranger"], **kwargs)
+
+
+def Rogue(**kwargs):
+    return Character(classs=CLASSES["rogue"], **kwargs)
+
+
+def Sorcerer(**kwargs):
+    return Character(classs=CLASSES["sorcerer"], **kwargs)
+
+
+def Warlock(**kwargs):
+    return Character(classs=CLASSES["warlock"], **kwargs)
+
+
+def Wizard(**kwargs):
+    return Character(classs=CLASSES["wizard"], **kwargs)
