b96c16aa9d9b97f1f9f6664ebf2ccd3534f7e037
tassaron2 <rutachigal@gmail.com>
2020-11-09 02:26:17 -0500

    version bump

--- dnd_character/__init__.py
+++ dnd_character/__init__.py
@@ -8,7 +8,7 @@ __author__ = "Brianna Rainey"
 __copyright__ = "Copyright 2019-2020 Brianna Rainey & Markis Cook"
 __credits__ = ["Brianna Rainey (Current Programmer)", "Markis Cook (Original Creator)"]
 __license__ = "EPL-2.0"
-__version__ = "20.11.08"
+__version__ = "20.11.09"
 __maintainer__ = "Brianna Rainey"
 
 
