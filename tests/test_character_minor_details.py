1c6fd75bb987ca9d11b37e36cd287476c96b5593
tassaron <rutachigal@gmail.com>
2022-04-17 21:53:52 -0400

    use pytest to check for exception

--- tests/test_character_minor_details.py
+++ tests/test_character_minor_details.py
@@ -1,16 +1,11 @@
-from ctypes import alignment
+import pytest
 from dnd_character import Character
 
 
 def test_alignment_is_2_characters():
     for length in (1, 3):
-        try:
-            print("t" * length)
+        with pytest.raises(AssertionError):
             new = Character(alignment="T" * length)
-            assert len(new.alignment) == 2
-        except AssertionError:
-            # test will pass if this exception is raised
-            pass
 
 
 def test_alignment_is_uppercase():
