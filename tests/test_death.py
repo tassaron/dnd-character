9da43713fbe021468e1cfb87dfe2f8ce22d0aeb4
tassaron <rutachigal@gmail.com>
2022-04-17 21:53:30 -0400

    add more tests for death saves and fails

--- tests/test_death.py
+++ tests/test_death.py
@@ -1,7 +1,10 @@
+from multiprocessing.sharedctypes import Value
 from dnd_character import Character
+import pytest
 
 
 def test_death_dying():
+    # dying happens when fails exceed saves
     c = Character()
     c.death_saves = 2
     c.death_fails = 3
@@ -9,7 +12,37 @@ def test_death_dying():
 
 
 def test_death_stabilizing():
+    # stabilizing (recovering from near-death) happens when saves exceed fails
     c = Character()
     c.death_fails = 2
     c.death_saves = 3
     assert c.dead == False and c.death_fails == 0
+
+
+def test_death_resets_death_fails():
+    c = Character()
+    c.death_fails = 2
+    c.dead = True
+    assert c.death_fails == 0
+
+
+def test_death_resets_death_saves():
+    c = Character()
+    c.death_saves = 2
+    c.dead = True
+    assert c.death_saves == 0
+
+
+def test_death_saves_range():
+    c = Character()
+    with pytest.raises(ValueError):
+        c.death_saves = 4
+        c.death_saves = -1
+
+
+def test_death_fails_range():
+    c = Character()
+    with pytest.raises(ValueError):
+        c.death_fails = 4
+        c.death_fails = -1
+
