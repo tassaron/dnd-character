85303a8b3b6ca403c0f1bcd6c80581fac9786d76
tassaron <rutachigal@gmail.com>
2022-04-18 19:15:39 -0400

    test str repr of character, and serialization of armour class and inventory

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,5 +1,6 @@
 from dnd_character import Character
 from dnd_character.SRD import SRD_classes
+from dnd_character.equipment import SRD_equipment
 from ast import literal_eval
 
 
@@ -88,3 +89,46 @@ def test_save_and_load_hitpoints():
     player = Character()
     player.hp = 6
     assert dict(Character(**dict(player))) == dict(player)
+
+
+def test_save_and_load_equipment():
+    player = Character()
+    player.giveItem(SRD_equipment["bagpipes"])
+    assert dict(Character(**dict(player))) == dict(player)
+
+
+def test_save_and_load_armour_class():
+    player = Character(dexterity=14)
+    assert dict(Character(**dict(player))) == dict(player)
+
+
+def test_save_and_load_light_armour_class():
+    player = Character(classs=SRD_classes["bard"], dexterity=14)
+    assert dict(Character(**dict(player))) == dict(player)
+
+
+def test_save_and_load_heavy_armour_class():
+    player = Character(classs=SRD_classes["paladin"], dexterity=14)
+    assert dict(Character(**dict(player))) == dict(player)
+
+
+def test_str_repr():
+    player = Character(classs=SRD_classes["fighter"])
+    player.giveItem(SRD_equipment["flute"])
+    player.experience += 50
+    character_repr = str(player)
+    mandatory_attrs = [
+        "name",
+        "age",
+        "gender",
+        "description",
+    ]
+    assert sum([f"{attr.title()}: {str(player.__dict__[attr])}" in character_repr for attr in mandatory_attrs]) == len(mandatory_attrs)
+    assert f"Biography:\n{str(player.biography)}" in character_repr
+    assert f"Class: {str(player.class_name)}" in character_repr
+    assert f"Level: {str(player.level)}" in character_repr
+    assert f"Experience: {str(player.experience)}" in character_repr
+    assert f"to next level: {str(player.experience.to_next_level)}" in character_repr.lower()
+    assert sum([value['name'] in character_repr for value in player.proficiencies.values()]) == len(player.proficiencies)
+    assert sum([value['name'] in character_repr for value in player.inventory]) == len(player.inventory)
+    assert sum([value['name'] in character_repr for value in player.class_features.values()]) == len(player.class_features)
