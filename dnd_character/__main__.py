ab1b5164110fa8caf577fa1818301b948a4f02f4
Brianna Rainey <brianna@tassaron.com>
2022-10-04 14:35:15 -0400

    add `--level` option to set class level from CLI

--- dnd_character/__main__.py
+++ dnd_character/__main__.py
@@ -11,18 +11,19 @@ def main():
   actions = parser.add_mutually_exclusive_group()
   actions.add_argument("-r", "--random", help="generate a random character", default=False, action="store_true")
   actions.add_argument("-c", "--class", help="generate a character with specified class", nargs=1, choices=CLASS_NAMES)
+  parser.add_argument("-l", "--level", help="set character to level", default="1", choices=[str(i) for i in range(1, 21)])
   args = parser.parse_args()
   char = None
 
   if args.random:
     import random
     classs = random.choice(CLASS_NAMES)
-    char = Character(classs=CLASSES[classs])
+    char = Character(classs=CLASSES[classs], level=int(args.level))
 
   # do gymnastics because class is a four letter word
   if args.__dict__["class"]:
     classs = args.__dict__["class"][0]
-    char = Character(classs=CLASSES[classs])
+    char = Character(classs=CLASSES[classs], level=int(args.level))
 
   if char:
     print(char)
