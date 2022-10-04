d06bed9920b53f9e4f012f509eb36134b7697fe7
Brianna Rainey <brianna@tassaron.com>
2022-10-04 14:49:19 -0400

    add `--format` option to CLI for output format such as text, dict, or json

--- dnd_character/__main__.py
+++ dnd_character/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+from pprint import pprint
 from dnd_character.character import Character
 from dnd_character.classes import CLASSES
 CLASS_NAMES = list(CLASSES.keys())
@@ -6,12 +8,13 @@ CLASS_NAMES = list(CLASSES.keys())
 def main():
   parser = argparse.ArgumentParser(
     prog="dnd-character",
-    description="generate D&D 5e character sheet as a text file from terminal"
+    description="generate D&D 5e character sheet from terminal"
   )
   actions = parser.add_mutually_exclusive_group()
   actions.add_argument("-r", "--random", help="generate a random character", default=False, action="store_true")
   actions.add_argument("-c", "--class", help="generate a character with specified class", nargs=1, choices=CLASS_NAMES)
   parser.add_argument("-l", "--level", help="set character to level", default="1", choices=[str(i) for i in range(1, 21)])
+  parser.add_argument("-f", "--format", help="output format", default="text", choices=["text", "dict", "json"])
   args = parser.parse_args()
   char = None
 
@@ -26,7 +29,12 @@ def main():
     char = Character(classs=CLASSES[classs], level=int(args.level))
 
   if char:
-    print(char)
+    if args.format == "text":
+      print(char)
+    elif args.format == "dict":
+      pprint(dict(char))
+    elif args.format == "json":
+      print(json.dumps(dict(char), indent=2))
   else:
     parser.print_help()
 
