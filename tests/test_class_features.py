2359d4555f4d054205b5bf5c0ad4312f73a4839e
TheOafidian <tim.van.rillaer@hotmail.com>
2023-04-28 17:48:12 +0200

    Update test to new API content
    leather -> leather-armor and bard college level ups at level 6 and 14 are merged into the same feature (therefore: 26-2 = 24)

--- tests/test_class_features.py
+++ tests/test_class_features.py
@@ -7,33 +7,33 @@ def test_class_features_apply_experience_gain():
     bard = Character(classs=CLASSES["bard"])
     for i in range(0, experience_at_level(20), 100):
         bard.experience += 100
-    assert len(bard.class_features) == 26
+    assert len(bard.class_features) == 24
 
 
 def test_class_features_apply_level_gain():
     bard = Character(classs=CLASSES["bard"])
     for i in range(20):
         bard.level += 1
-    assert len(bard.class_features) == 26
+    assert len(bard.class_features) == 24
 
 
 def test_class_features_apply_experience_jump():
     bard = Character(classs=CLASSES["bard"])
     bard.experience = experience_at_level(20)
-    assert len(bard.class_features) == 26
+    assert len(bard.class_features) == 24
 
 
 def test_class_features_apply_level_jump():
     bard = Character(classs=CLASSES["bard"])
     bard.level = 20
-    assert len(bard.class_features) == 26
+    assert len(bard.class_features) == 24
 
 
 def test_class_features_apply_high_experience_initialized():
     bard = Character(classs=CLASSES["bard"], experience=experience_at_level(20))
-    assert len(bard.class_features) == 26
+    assert len(bard.class_features) == 24
 
 
 def test_class_features_apply_high_level_initialized():
     bard = Character(classs=CLASSES["bard"], level=20)
-    assert len(bard.class_features) == 26
+    assert len(bard.class_features) == 24
