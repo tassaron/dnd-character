1acfec1501abe374b5febb6076b611c58e8834fc
Brianna Rainey <brianna@tassaron.com>
2023-07-12 11:25:59 -0400

    bump version

--- dnd_character/__init__.py
+++ dnd_character/__init__.py
@@ -8,7 +8,7 @@ __author__ = "Brianna Rainey"
 __copyright__ = "Copyright 2019-2023 Brianna Rainey & Markis Cook"
 __credits__ = ["Brianna Rainey (Current Programmer)", "Markis Cook (Original Creator)"]
 __license__ = "EPL-2.0"
-__version__ = "23.07.06"
+__version__ = "23.07.12"
 __maintainer__ = "Brianna Rainey"
 
 
