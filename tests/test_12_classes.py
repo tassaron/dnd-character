b9edea92ffeb0e20656a8416886b9c7bd30346fa
Brianna Rainey <brianna@tassaron.com>
2023-07-22 17:05:38 -0400

    test classes slightly better

--- tests/test_12_classes.py
+++ tests/test_12_classes.py
@@ -1,4 +1,5 @@
 from dnd_character.classes import (
+    CLASSES,
     Barbarian,
     Bard,
     Cleric,
@@ -16,15 +17,26 @@ from dnd_character.classes import (
 
 def test_all_12_classes():
     barbarian = Barbarian()
+    assert barbarian.classs == CLASSES["barbarian"]
     bard = Bard()
+    assert bard.classs == CLASSES["bard"]
     cleric = Cleric()
+    assert cleric.classs == CLASSES["cleric"]
     druid = Druid()
+    assert druid.classs == CLASSES["druid"]
     fighter = Fighter()
+    assert fighter.classs == CLASSES["fighter"]
     monk = Monk()
+    assert monk.classs == CLASSES["monk"]
     paladin = Paladin()
+    assert paladin.classs == CLASSES["paladin"]
     ranger = Ranger()
+    assert ranger.classs == CLASSES["ranger"]
     rogue = Rogue()
+    assert rogue.classs == CLASSES["rogue"]
     sorcerer = Sorcerer()
+    assert sorcerer.classs == CLASSES["sorcerer"]
     warlock = Warlock()
+    assert warlock.classs == CLASSES["warlock"]
     wizard = Wizard()
-    assert wizard.classs
+    assert wizard.classs == CLASSES["wizard"]
