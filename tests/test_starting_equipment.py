1a93a4ef4d531b1144609bcadad00148934395b9
Brianna Rainey <brianna@tassaron.com>
2023-07-26 13:23:28 -0400

    add test of Monk inventory with starting_equipment rename other tests in this file for accuracy

--- tests/test_starting_equipment.py
+++ tests/test_starting_equipment.py
@@ -1,7 +1,8 @@
+from ast import literal_eval
 from dnd_character.classes import *
 
 
-def test_starting_equipment_barbarian():
+def test_player_options_barbarian():
     assert Barbarian().player_options["starting_equipment"] == [
         "choose from 1 x Greataxe, 1 x Martial Melee Weapons (choice from Battleaxe, Flail, "
         "Glaive, Greataxe, Greatsword, Halberd, Lance, Longsword, Maul, Morningstar, Pike, "
@@ -12,7 +13,7 @@ def test_starting_equipment_barbarian():
     ]
 
 
-def test_starting_equipment_bard():
+def test_player_options_bard():
     assert Bard().player_options["starting_equipment"] == [
         "choose from 1 x Rapier, 1 x Longsword, 1 x Simple Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear, Crossbow, light, Dart, Shortbow, Sling)",
         "choose from 1 x Diplomat's Pack, 1 x Entertainer's Pack",
@@ -20,7 +21,7 @@ def test_starting_equipment_bard():
     ]
 
 
-def test_starting_equipment_cleric():
+def test_player_options_cleric():
     assert Cleric().player_options["starting_equipment"] == [
         "choose from 1 x Mace, 1 x Warhammer",
         "choose from 1 x Scale Mail, 1 x Leather Armor, 1 x Chain Mail",
@@ -31,7 +32,7 @@ def test_starting_equipment_cleric():
     ]
 
 
-def test_starting_equipment_druid():
+def test_player_options_druid():
     assert Druid().player_options["starting_equipment"] == [
         "choose from 1 x Shield, 1 x Simple Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear, Crossbow, light, Dart, Shortbow, Sling)",
         "choose from 1 x Scimitar, 1 x Simple Melee Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear)",
@@ -39,7 +40,7 @@ def test_starting_equipment_druid():
     ]
 
 
-def test_starting_equipment_fighter():
+def test_player_options_fighter():
     assert Fighter().player_options["starting_equipment"] == [
         "1 Leather Armor, 1 Longbow, 20 Arrow",
         "choose from 1 x Chain Mail",
@@ -52,13 +53,23 @@ def test_starting_equipment_fighter():
 
 
 def test_starting_equipment_monk():
+    inv = dict(Monk())["inventory"]
+    assert inv[0]["index"] == "dart" and inv[0]["quantity"] == 10
+
+
+def test_starting_equipment_monk_serialized():
+    inv = dict(Character(**literal_eval(str(dict(Monk())))))["inventory"]
+    assert inv[0]["index"] == "dart" and inv[0]["quantity"] == 10
+
+
+def test_player_options_monk():
     assert Monk().player_options["starting_equipment"] == [
         "choose from 1 x Shortsword, 1 x Simple Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear, Crossbow, light, Dart, Shortbow, Sling)",
         "choose from 1 x Dungeoneer's Pack, 1 x Explorer's Pack",
     ]
 
 
-def test_starting_equipment_paladin():
+def test_player_options_paladin():
     assert Paladin().player_options["starting_equipment"] == [
         "choose 1 from Martial Weapons (choice from Battleaxe, Flail, Glaive, Greataxe, Greatsword, Halberd, Lance, Longsword, Maul, Morningstar, Pike, Rapier, Scimitar, Shortsword, Trident, War pick, Warhammer, Whip, Blowgun, Crossbow, hand, Crossbow, heavy, Longbow, Net) or a Shield",
         "choose from 2 x Martial Weapons (choice from Battleaxe, Flail, Glaive, Greataxe, Greatsword, Halberd, Lance, Longsword, Maul, Morningstar, Pike, Rapier, Scimitar, Shortsword, Trident, War pick, Warhammer, Whip, Blowgun, Crossbow, hand, Crossbow, heavy, Longbow, Net)",
@@ -68,7 +79,7 @@ def test_starting_equipment_paladin():
     ]
 
 
-def test_starting_equipment_ranger():
+def test_player_options_ranger():
     assert Ranger().player_options["starting_equipment"] == [
         "choose from 1 x Scale Mail, 1 x Leather Armor",
         "choose from 2 x Shortsword, 2 x Simple Melee Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear)",
@@ -76,7 +87,7 @@ def test_starting_equipment_ranger():
     ]
 
 
-def test_starting_equipment_rogue():
+def test_player_options_rogue():
     assert Rogue().player_options["starting_equipment"] == [
         "choose from 1 x Rapier, 1 x Shortsword",
         "1 Shortbow, 20 Arrow",
@@ -85,7 +96,7 @@ def test_starting_equipment_rogue():
     ]
 
 
-def test_starting_equipment_sorcerer():
+def test_player_options_sorcerer():
     assert Sorcerer().player_options["starting_equipment"] == [
         "1 Crossbow, light, 20 Crossbow bolt",
         "choose from 1 x Simple Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear, Crossbow, light, Dart, Shortbow, Sling)",
@@ -94,7 +105,7 @@ def test_starting_equipment_sorcerer():
     ]
 
 
-def test_starting_equipment_warlock():
+def test_player_options_warlock():
     assert Warlock().player_options["starting_equipment"] == [
         "1 Crossbow, light, 20 Crossbow bolt",
         "choose from 1 x Simple Weapons (choice from Club, Dagger, Greatclub, Handaxe, Javelin, Light hammer, Mace, Quarterstaff, Sickle, Spear, Crossbow, light, Dart, Shortbow, Sling)",
@@ -104,7 +115,7 @@ def test_starting_equipment_warlock():
     ]
 
 
-def test_starting_equipment_wizard():
+def test_player_options_wizard():
     assert Wizard().player_options["starting_equipment"] == [
         "choose from 1 x Quarterstaff, 1 x Dagger",
         "choose from 1 x Component pouch, 1 x Arcane Foci (choice from Crystal, Orb, Rod, Staff, Wand)",
